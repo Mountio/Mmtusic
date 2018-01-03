2602498690fe2c76dbc70d904cabb0976e2f65ea
amazinigmech2418 <33498997+amazinigmech2418@users.noreply.github.com>
2018-01-03 17:46:21 -0500

    Update load.js

--- load.js
+++ load.js
@@ -1,1 +1,14 @@
+function download(info,name) {
+  var el = document.createElement("A");
+  el.setAttribute("href","data:application,"+info);
+  el.setAttribute("download",name);    
+  if (document.createEvent) {
+    var event = document.createEvent('MouseEvents');
+    event.initEvent('click', true, true);
+    el.dispatchEvent(event);
+  }
+  else {
+    el.click();
+  }
+}
 
